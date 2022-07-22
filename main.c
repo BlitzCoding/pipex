6445401e98ed8c7271e35211e1cf3621f1e69430
Yonghun Lee <yonghlee@c5r5s7.42seoul.kr>
2022-07-22 14:12:30 +0900

    dup

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yonghlee <yonghlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 11:31:06 by yonghlee          #+#    #+#             */
-/*   Updated: 2022/07/22 13:42:10 by yonghlee         ###   ########.fr       */
+/*   Updated: 2022/07/22 14:12:21 by yonghlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,22 @@ char	*attach_path_cmd(char *cmd, t_pipex *pipex)
 	char	*res_cmd;
 	int		fd;
 
-	i = 0;
+	i = -1;
 	path_cmd = ft_strjoin("/", cmd);
-	while (pipex->envp_parse[i])
+	while (pipex->envp_parse[++i])
 	{
 		res_cmd = ft_strjoin(pipex->envp_parse[i], path_cmd);
-		printf("%s\n", res_cmd);
-		i++;
+		fd = access(res_cmd, X_OK);
+		if (fd != -1)
+		{
+			free(path_cmd);
+			return (res_cmd);
+		}
+		close(fd);
+		free(res_cmd);
 	}
-	return (res_cmd);
+	free(path_cmd);
+	return (NULL);
 }
 
 void	argv_parse(t_pipex *pipex, char **argv)
@@ -57,12 +64,23 @@ void	envp_parse(t_pipex *pipex, char **envp)
 
 void	init_pipex(t_pipex *pipex, char **argv, char **envp)
 {
+	int	fd1;
+	int	fd2;
+
 	pipex->infile = ft_strdup(argv[1]);
 	pipex->outfile = ft_strdup(argv[4]);
 	if (pipex->infile == NULL || pipex->outfile == NULL)
 		print_error(PARSE_FAIL, pipex);
 	envp_parse(pipex, envp);
 	argv_parse(pipex, argv);
+	fd1 = open(pipex->infile, O_RDONLY);
+	fd2 = open(pipex->outfile, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (fd1 < 0 || fd2 < 0)
+		print_error(OPEN_FAIL, pipex);
+	dup2(fd1, STDIN_FILENO);
+	dup2(fd2, STDOUT_FILENO);
+	close(fd1);
+	close(fd2);
 }
 
 int main(int argc, char **argv, char **envp)
@@ -72,4 +90,6 @@ int main(int argc, char **argv, char **envp)
 	if (argc != 5)
 		print_error(INPUT_FAIL, &pipex);
 	init_pipex(&pipex, argv, envp);
+	getchar();
+	system("leaks pipex");
 }
